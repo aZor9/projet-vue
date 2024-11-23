e117aed071542ce9f7ea0e7ea05aa2e79dfdd3f6
Hugo <hugo.lembrez@gmail.com>
2024-11-23 23:54:25 +0100

    creation des components

---
+++ src/components/TestImageComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{ msg }}
+        </h1>
+        <img src="" alt="">
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            msg: 'Hello World'
+        }
+    }
+}
+</script>
